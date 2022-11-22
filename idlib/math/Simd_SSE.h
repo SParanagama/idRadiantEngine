a3b95a1fda569ece8f9c31ce48b68563e32eaf1f
Sagara Paranagama <sagara89dev@gmail.com>
2022-11-23 09:30:06 +1100

    Add darkmod

---
+++ idlib/math/Simd_SSE.h
@@ -0,0 +1,39 @@
+/*****************************************************************************
+The Dark Mod GPL Source Code
+
+This file is part of the The Dark Mod Source Code, originally based
+on the Doom 3 GPL Source Code as published in 2011.
+
+The Dark Mod Source Code is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version. For details, see LICENSE.TXT.
+
+Project: The Dark Mod (http://www.thedarkmod.com/)
+
+******************************************************************************/
+
+#ifndef __MATH_SIMD_SSE_H__
+#define __MATH_SIMD_SSE_H__
+
+#include "Simd_Generic.h"
+
+/*
+===============================================================================
+
+	SSE implementation of idSIMDProcessor
+
+===============================================================================
+*/
+
+class idSIMD_SSE : public idSIMD_Generic {
+public:
+	idSIMD_SSE();
+
+#ifdef ENABLE_SSE_PROCESSORS
+	virtual void CullByFrustum( idDrawVert *verts, const int numVerts, const idPlane frustum[6], byte *pointCull, float epsilon );
+	virtual void CullByFrustum2( idDrawVert *verts, const int numVerts, const idPlane frustum[6], unsigned short *pointCull, float epsilon );
+#endif
+};
+
+#endif /* !__MATH_SIMD_SSE_H__ */
